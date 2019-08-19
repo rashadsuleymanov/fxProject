e2cfed7124d61f8af2e294db56d813c6166ef8bb
Rashad Suleymanov <resad.suleymanov.811@mail.ru>
2019-08-19 17:12:44 +0400

    yess

--- N.Bank/src/az/naxpoct/bank/controller/MainClass.java
+++ N.Bank/src/az/naxpoct/bank/controller/MainClass.java
@@ -20,7 +20,7 @@ import javafx.stage.StageStyle;
  * @author Rashad Suleymanov
  */
 public class MainClass extends Application {
-    
+    //yess adlindi!
     private double xOffset = 0;
     private double yOffset = 0;
     
